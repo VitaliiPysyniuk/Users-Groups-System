7b2ba15236e6856742ace91d7406ec589478bee7
VitaliiPysyniuk <vitaliy2112@gmail.com>
2022-09-28 22:06:49 +0300

    Add unique parameter to the name field of the Group table

--- apps/groups/models.py
+++ apps/groups/models.py
@@ -7,7 +7,7 @@ class GroupModel(models.Model):
     class Meta:
         db_table = 'groups'
 
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, unique=True)
     description = models.CharField(max_length=100)
 
     users = models.ManyToManyField(UsedModel, related_name='groups')
