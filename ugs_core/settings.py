648aef9c23e51e04906830e7f893080572ad97b1
VitaliiPysyniuk <vitaliy2112@gmail.com>
2022-09-28 22:23:23 +0300

    Add usage of secrets in the CI workflow

--- ugs_core/settings.py
+++ ugs_core/settings.py
@@ -12,10 +12,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
-from dotenv import load_dotenv
-
-load_dotenv('.env')
-load_dotenv('.env.db')
+# from dotenv import load_dotenv
+#
+# load_dotenv('.env')
+# load_dotenv('.env.db')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
