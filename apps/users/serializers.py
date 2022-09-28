a8e75ae241810f66c8354c46bfc051c57ba302dc
VitaliiPysyniuk <vitaliy2112@gmail.com>
2022-09-28 13:40:34 +0300

    Add two different serializers for user create/update and user list endpoints

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -4,10 +4,13 @@ from .models import UsedModel
 from ..groups.serializers import GroupSerializer
 
 
-class UserSerializer(ModelSerializer):
-    # groups = GroupSerializer(required=False, many=True)
+class UserCreateUpdateSerializer(ModelSerializer):
 
     class Meta:
         model = UsedModel
         fields = ['id', 'email', 'username', 'is_admin', 'created_at', 'groups']
-        extra_kwargs = {'created_at': {'read_only': True}}
+        extra_kwargs = {'created_at': {'read_only': True}, 'groups': {'required': False}}
+
+
+class UserListSerializer(UserCreateUpdateSerializer):
+    groups = GroupSerializer(many=True)
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,17 +1,21 @@
 from rest_framework.generics import ListCreateAPIView, UpdateAPIView, DestroyAPIView
 
 from .models import UsedModel
-from .serializers import UserSerializer
+from .serializers import UserCreateUpdateSerializer, UserListSerializer
 
 
 class UserListCreateView(ListCreateAPIView):
     queryset = UsedModel.objects.all().order_by('id')
-    serializer_class = UserSerializer
+    serializer_class = UserCreateUpdateSerializer
+
+    def get(self, request, *args, **kwargs):
+        self.serializer_class = UserListSerializer
+        return super().get(request, *args, **kwargs)
 
 
 class UserUpdateDestroyView(UpdateAPIView, DestroyAPIView):
     queryset = UsedModel.objects.all()
-    serializer_class = UserSerializer
+    serializer_class = UserCreateUpdateSerializer
     lookup_field = 'id'
 
 
